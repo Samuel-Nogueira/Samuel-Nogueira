81443c389fcba01d21d34eb9d3954d4348d7adab
Samuel Nogueira <105369917+Samuel-Nogueira@users.noreply.github.com>
2022-06-02 17:55:08 -0700

    limpeza
    limpeza do script

--- ETL/SQL/Fatiamento/Script/tb_fatiamento_stg.sql
+++ ETL/SQL/Fatiamento/Script/tb_fatiamento_stg.sql
@@ -1,13 +1,3 @@
-USE [COMERCIO_OLTP]
-GO
-
-/****** Object:  Table [dbo].[tb_fatiamento_stg]    Script Date: 6/2/2022 5:52:16 PM ******/
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
-
 CREATE TABLE [dbo].[tb_fatiamento_stg](
 	[Coluna] [varchar](max) NULL
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
